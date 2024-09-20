94b8ddb525f4a76ce83846cbe1da3754661d3740
Savio Ferreira <saviofc.dev@gmail.com>
2024-09-20 09:56:37 -0300

    representacao do polimorfismo

--- polimorfismo/exercicio-taxaudit/src/main/java/LegalApp.java
+++ polimorfismo/exercicio-taxaudit/src/main/java/LegalApp.java
@@ -7,11 +7,18 @@ public class LegalApp {
     public static void main(String []args) {
         System.out.println("Tax Audit");
         var taxManager = new TaxManager();
+
+        var marlon = new NaturalPerson("Marlon mendonça gonsalvea", 35_000);
+        var eduard = new NaturalPerson("Eduard Newgates", 200_000);
+
         var portegas = new RealCompanyProfit("Portegas D. Ace", 4_000_000, 700_000);
         var valur = new SimpleCompany("Valur D. Valíria", 500_000, 130_000);
 
         taxManager.add(portegas);
         taxManager.add(valur);
+
+        taxManager.add(marlon);
+        taxManager.add(eduard);
 
     }
 }
--- polimorfismo/exercicio-taxaudit/src/main/java/entity/TaxManager.java
+++ polimorfismo/exercicio-taxaudit/src/main/java/entity/TaxManager.java
@@ -5,6 +5,7 @@ import entity.person.Person;
 public class TaxManager {
     private double totalTaxValue;
 
+
     public void add(Person person) {
         System.out.println(person.getName() + ": " + person.taxCalculate());
         totalTaxValue += person.taxCalculate();
