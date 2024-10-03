3257afef24ea3545cbebde4c3839072fd2b842a6
Savio Ferreira <saviofc.dev@gmail.com>
2024-10-03 11:17:35 -0300

    incluindo metodos para validar status

--- enum/exercicioEnum/src/NotaFiscal.java
+++ enum/exercicioEnum/src/NotaFiscal.java
@@ -3,7 +3,7 @@ public class NotaFiscal {
     private final Integer numero;
     private final String descricao;
     private final double valor;
-    private NotaFiscalEnum status = NotaFiscalEnum.NAO_EMITIDA;
+    private StatusNotaFiscalEnum status = StatusNotaFiscalEnum.NAO_EMITIDA;
 
     public NotaFiscal(Integer numero, String descricao, double valor) {
         this.numero = numero;
@@ -23,30 +23,29 @@ public class NotaFiscal {
         return valor;
     }
 
-    public NotaFiscalEnum getStatus() {
+    public StatusNotaFiscalEnum getStatus() {
         return status;
     }
 
     public void cancelar() {
-        if ((status == NotaFiscalEnum.EMITIDA && getValor() >= 1_000)
-                || status == NotaFiscalEnum.CANCELADA) {
+        if (getStatus().podeMudarParaCancelado(getValor())) {
+            status = StatusNotaFiscalEnum.CANCELADA;
+        } else {
             throw new IllegalStateException("Não foi possível cancelar a nota fiscal");
         }
-
-        status = NotaFiscalEnum.CANCELADA;
     }
 
     public void emitir() {
-        if (status == NotaFiscalEnum.EMITIDA || status == NotaFiscalEnum.CANCELADA) {
+        if (getStatus().podeMudarParaEmitido(getValor())) {
+            status = StatusNotaFiscalEnum.EMITIDA;
+        } else {
             throw new IllegalStateException("Não foi possível emitir a nota fiscal");
         }
-
-        status = NotaFiscalEnum.EMITIDA;
     }
 
     public String getDescricaoCompleta() {
         return String.format("Nota fiscal #%d (%s) no valor de R$%.2f está %s",
-                getNumero(), getDescricao(), getValor(), getStatus());
+                getNumero(), getDescricao(), getValor(), getStatus().getDescricao());
     }
 
 }
